86e4df1538f25fff64427f22efd30e55a8e66620
Alastair Moore <alastair@fullfatthings.com>
2019-06-04 17:22:15 +0100

    Added reference url and injecting username/password from .env file

--- backstop_data/engine_scripts/onBeforeScript.js
+++ backstop_data/engine_scripts/onBeforeScript.js
@@ -2,7 +2,12 @@ const dotenv = require('dotenv').config();
 
 module.exports = async (page, scenario, vp) => {
   await page.goto(process.env.URL + '/user');
-  await page.type('#edit-name', '');
-  await page.type('#edit-pass', '');
+  await page.type('#edit-name', process.env.USERNAME);
+  await page.type('#edit-pass', process.env.PASSWORD);
+  await page.click('#edit-submit');
+
+  await page.goto(process.env.REFERENCE_URL + '/user');
+  await page.type('#edit-name', process.env.USERNAME);
+  await page.type('#edit-pass', process.env.PASSWORD);
   await page.click('#edit-submit');
 };
